18fea3e3e6589d0b34c830b1627e1267a276ac7f
bronsetn <32513753+kaffefaen@users.noreply.github.com>
2020-11-30 18:02:19 +0100

    reinstallation of vue without prettier

--- erlendweb/src/App.vue
+++ erlendweb/src/App.vue
@@ -1,29 +1,32 @@
 <template>
   <div id="app">
-    <v-app-bar>
-      <v-btn text @click="$router.push('/Home')" color="primary">Home</v-btn>
-      <v-btn text @click="$router.push('/About')" color="primary">About </v-btn>
-      <v-btn text @click="$router.push('/Forecast')" color="primary">Forecast</v-btn>
-    </v-app-bar>
-    <router-view />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-<script>
-export default {
-  name: "App",
-};
-</script>
+
 <style>
 #app {
-  font-family: "Roboto Condensed", sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  color: #2c3e50;
 }
 
-.center {
-  display: flex;
-  justify-content: center;
-  align-items: center;
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- erlendweb/src/router/index.js
+++ erlendweb/src/router/index.js
@@ -1,34 +1,27 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
 
-import Home from "../views/Home.vue";
-import About from "../views/About.vue";
-import Forecast from "../views/Forecast.vue";
-
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'Home',
     component: Home
   },
   {
-    path: "/About",
-    name: "About",
-    component: About
-  },
-  {
-    path: "/Forecast",
-    name: "Forecast",
-    component: Forecast
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
-];
+]
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
   routes
-});
+})
 
-export default router;
+export default router
