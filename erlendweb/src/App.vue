01c2caeebaa56afe8bc109ef5bbc72956d3b8c9b
bronsetn <32513753+kaffefaen@users.noreply.github.com>
2020-11-30 19:08:19 +0100

    +

--- erlendweb/src/App.vue
+++ erlendweb/src/App.vue
@@ -2,11 +2,10 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/forecast">Forecast</router-link>
     </div>
-    <v-card>
-      <router-view />
-    </v-card>
+    <router-view />
   </div>
 </template>
 
--- erlendweb/src/views/Forecast.vue
+++ erlendweb/src/views/Forecast.vue
@@ -1,1 +1,12 @@
+<template>
+  <body>
+    <v-card> jajalaåls </v-card>
+  </body>
+</template>
 
+<script>
+export default {};
+</script>
+
+<style>
+</style>
